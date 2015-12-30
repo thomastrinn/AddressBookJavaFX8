722b95ead09799311312540bfd10b7f600e78f45
thomas <thomas.trinn@gmail.com>
2015-12-30 20:26:57 +0100

    Add Rows to the Table

--- src/main/java/com/thomas/addressbook/AddressBookController.java
+++ src/main/java/com/thomas/addressbook/AddressBookController.java
@@ -1,13 +1,40 @@
 package com.thomas.addressbook;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author thomas
  */
 public class AddressBookController {
-    
+
+    @FXML
+    private TableView<PersonDataModel> adressBookTableView;
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    protected void addPerson(ActionEvent event) {
+        ObservableList<PersonDataModel> data = adressBookTableView.getItems();
+        data.add(new PersonDataModel(
+                firstNameField.getText(),
+                lastNameField.getText(),
+                emailField.getText()
+        ));
+
+        firstNameField.setText("");
+        lastNameField.setText("");
+        emailField.setText("");
+    }
+
     @FXML
     protected void initialize() {
         
